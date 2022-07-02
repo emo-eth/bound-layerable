66370060530c31512fc1f9fd9918a3c22f916286
James Wenzel <wenzel.james.r@gmail.com>
2022-07-02 00:17:11 -0700

    update gitignore, move deploy

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -5,7 +5,7 @@ import {Test} from 'forge-std/Test.sol';
 import {Token} from 'bound-layerable/test/Token.sol';
 
 contract Deploy is Test {
-    function deploy() public {
+    function run() public {
         vm.broadcast(0x92B381515bd4851Faf3d33A161f7967FD87B1227);
         Token token = new Token('test', 'test');
         token.mintSet();
