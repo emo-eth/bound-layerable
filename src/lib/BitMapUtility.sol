0a2a545c4b70b140ddb64802879e6f95abc6d94c
James Wenzel <wenzel.james.r@gmail.com>
2022-07-05 21:24:13 -0700

    move constants

--- src/lib/BitMapUtility.sol
+++ src/lib/BitMapUtility.sol
@@ -1,23 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-uint256 constant _2_128 = 2**128;
-uint256 constant _2_64 = 2**64;
-uint256 constant _2_32 = 2**32;
-uint256 constant _2_16 = 2**16;
-uint256 constant _2_8 = 2**8;
-uint256 constant _2_4 = 2**4;
-uint256 constant _2_2 = 2**2;
-uint256 constant _2_1 = 2**1;
-
-uint256 constant _128_MASK = 2**128 - 1;
-uint256 constant _64_MASK = 2**64 - 1;
-uint256 constant _32_MASK = 2**32 - 1;
-uint256 constant _16_MASK = 2**16 - 1;
-uint256 constant _8_MASK = 2**8 - 1;
-uint256 constant _4_MASK = 2**4 - 1;
-uint256 constant _2_MASK = 2**2 - 1;
-uint256 constant _1_MASK = 2**1 - 1;
+import '../interface/Constants.sol';
 
 library BitMapUtility {
     /**
