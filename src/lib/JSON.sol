a12b996e5e3630da21d318033842b156fcf8c035
James Wenzel <james.wenzel@opensea.io>
2022-07-13 15:06:06 -0700

    add natspec to json lib

--- src/lib/JSON.sol
+++ src/lib/JSON.sol
@@ -6,14 +6,30 @@ import {Strings} from 'openzeppelin-contracts/utils/Strings.sol';
 library json {
     using Strings for uint256;
 
+    /**
+     * @notice enclose a string in {braces}
+     * @param  value string to enclose in braces
+     * @return string of {value}
+     */
     function object(string memory value) internal pure returns (string memory) {
         return string.concat('{', value, '}');
     }
 
+    /**
+     * @notice enclose a string in [brackets]
+     * @param value string to enclose in brackets
+     * @return string of [value]
+     */
     function array(string memory value) internal pure returns (string memory) {
         return string.concat('[', value, ']');
     }
 
+    /**
+     * @notice enclose name and value with quotes, and place a colon "between":"them"
+     * @param name name of property
+     * @param value value of property
+     * @return string of "name":"value"
+     */
     function property(string memory name, string memory value)
         internal
         pure
@@ -22,6 +38,11 @@ library json {
         return string.concat('"', name, '":"', value, '"');
     }
 
+    /**
+     * @notice comma-join an array of properties and {"enclose":"them","in":"braces"}
+     * @param properties array of properties to join
+     * @return string of {"name":"value","name":"value",...}
+     */
     function objectOf(string[] memory properties)
         internal
         pure
@@ -37,6 +58,11 @@ library json {
         return object(result);
     }
 
+    /**
+     * @notice comma-join an array of values and enclose them [in,brackets]
+     * @param values array of values to join
+     * @return string of [value,value,...]
+     */
     function arrayOf(string[] memory values)
         internal
         pure
@@ -45,6 +71,12 @@ library json {
         return array(_commaJoin(values));
     }
 
+    /**
+     * @notice comma-join two arrays of values and [enclose,them,in,brackets]
+     * @param values1 first array of values to join
+     * @param values2 second array of values to join
+     * @return string of [values1_0,values1_1,values2_0,values2_1...]
+     */
     function arrayOf(string[] memory values1, string[] memory values2)
         internal
         pure
@@ -59,29 +91,46 @@ library json {
             array(string.concat(_commaJoin(values1), ',', _commaJoin(values2)));
     }
 
+    /**
+     * @notice enclose a string in double "quotes"
+     * @param str string to enclose in quotes
+     * @return string of "value"
+     */
     function quote(string memory str) internal pure returns (string memory) {
         return string.concat('"', str, '"');
     }
 
+    /**
+     * @notice comma-join an array of strings
+     * @param values array of strings to join
+     * @return string of value,value,...
+     */
     function _commaJoin(string[] memory values)
         internal
         pure
-        returns (string memory result)
+        returns (string memory)
     {
         return _join(values, ',');
     }
 
+    /**
+     * @notice join an array of strings with a specified separator
+     * @param values array of strings to join
+     * @param separator separator to join with
+     * @return string of value<separator>value<separator>...
+     */
     function _join(string[] memory values, string memory separator)
         internal
         pure
-        returns (string memory result)
+        returns (string memory)
     {
         if (values.length == 0) {
             return '';
         }
-        result = values[0];
+        string memory result = values[0];
         for (uint256 i = 1; i < values.length; ++i) {
             result = string.concat(result, separator, values[i]);
         }
+        return result;
     }
 }
